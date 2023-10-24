1b3d53816d125ff42d3d033118ac1bb3330fc196
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-24 17:04:05 +0300

    create get api's express

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { chats } = require("./data/data");
 
 const app = express();
 
@@ -6,4 +7,13 @@ app.listen(5000, console.log("Server started at PORT 5000"));
 
 app.get("/", (req, res) => {
   res.send("My first API running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 });
