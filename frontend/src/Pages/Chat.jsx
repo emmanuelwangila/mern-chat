0b8863142ccca15cd380b615502978f83d581d18
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-27 17:42:22 +0300

    render data from back-end

--- frontend/src/Pages/Chat.jsx
+++ frontend/src/Pages/Chat.jsx
@@ -1,15 +1,27 @@
 // import React, { useEffect } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Chat() {
+  const [chats, setChats] = useState([]);
+
   const fetchChats = async () => {
     const { data } = await axios.get("/api/chat");
-    console.log(data);
+    setChats(data);
   };
 
-  fetchChats();
+  useEffect(() => {
+    fetchChats();
+  }, []);
 
-  return <div>Chat</div>;
+  return (
+    <div>
+      {" "}
+      {chats.map((chat) => (
+        <div> {chat.chatName} </div>
+      ))}{" "}
+    </div>
+  );
 }
 
 export default Chat;
