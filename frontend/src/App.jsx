792184abfedbae856cf8de5aac85269d248fde68
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-27 17:26:05 +0300

    fetch data from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 // import { Button } from "@chakra-ui/react";
 import { Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Chat from "./Pages/Chat";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Home} />
-      <Route path="/chats" />
+      <Route path="/" component={Home} exact />
+      <Route path="/chats" component={Chat} />
       <Route path="/" />
     </div>
   );
--- frontend/src/Pages/Chat.jsx
+++ frontend/src/Pages/Chat.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+// import React, { useEffect } from "react";
+import axios from "axios";
 
 function Chat() {
+  const fetchChats = async () => {
+    const { data } = await axios.get("/api/chat");
+    console.log(data);
+  };
+
+  fetchChats();
+
   return <div>Chat</div>;
 }
 
