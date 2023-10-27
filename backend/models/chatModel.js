53e31d7f3eb857bcd6a75fb573a16a08041a3efc
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-27 18:13:52 +0300

    adding models message chats

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -3,4 +3,15 @@ const mongoose = require("mongoose");
 const chatModel = mongoose.Schema({
   chatName: { type: String, trim: true },
   isGroupChat: { type: Boolean, default: false },
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  latestMessage: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Message",
+  },
 });
