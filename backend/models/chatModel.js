b163492255a173afc3d0a0fa6395074c52238a66
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-27 18:18:04 +0300

    adding timestamp and admin models

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,17 +1,28 @@
 const mongoose = require("mongoose");
 
-const chatModel = mongoose.Schema({
-  chatName: { type: String, trim: true },
-  isGroupChat: { type: Boolean, default: false },
-  users: [
-    {
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+
+    groupAdmin: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-  ],
-
-  latestMessage: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Message",
   },
-});
+
+  {
+    timestamps: true,
+  }
+);
