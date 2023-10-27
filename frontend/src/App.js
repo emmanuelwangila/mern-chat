6735f31b4a53a3dd6efc4a85b6a27d67513ff287
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-27 16:11:04 +0300

    Chakra Ui

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
+}
+
+export default App;
