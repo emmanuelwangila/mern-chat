ac4522a1557525d201dad99ba2fc9b7b2ded6c24
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-31 09:18:49 +0300

    setting up states

--- frontend/src/components/Auth/SignUp.jsx
+++ frontend/src/components/Auth/SignUp.jsx
@@ -1,7 +1,21 @@
+import { FormControl, FormLabel, VStack, Input } from "@chakra-ui/react";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [name, setname] = useState();
+  const [email, setemail] = useState();
+  const [password, setpassword] = useState();
+  const [confirmpassword, setconfirmpassword] = useState();
+  const [pic, setpic] = useState();
+
+  return (
+    <VStack spacing={"5px"}>
+      <FormControl>
+        <FormLabel></FormLabel>
+        <Input placeholder={"Enter your Name"} />
+      </FormControl>
+    </VStack>
+  );
 };
 
 export default SignUp;
