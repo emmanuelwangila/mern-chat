54c6f89a4d7ad72d17c6d5332b0d8c82429214fe
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-31 09:27:20 +0300

    event listener on change

--- frontend/src/components/Auth/SignUp.jsx
+++ frontend/src/components/Auth/SignUp.jsx
@@ -1,18 +1,22 @@
 import { FormControl, FormLabel, VStack, Input } from "@chakra-ui/react";
 import React from "react";
+import { useState } from "react";
 
 const SignUp = () => {
-  const [name, setname] = useState();
+  const [name, setName] = useState();
   const [email, setemail] = useState();
   const [password, setpassword] = useState();
   const [confirmpassword, setconfirmpassword] = useState();
   const [pic, setpic] = useState();
 
   return (
-    <VStack spacing={"5px"}>
-      <FormControl>
+    <VStack spacing={"5px"} color={"gray.200"}>
+      <FormControl id="FirstName" isRequired>
         <FormLabel></FormLabel>
-        <Input placeholder={"Enter your Name"} />
+        <Input
+          placeholder={"Enter your Name"}
+          onChange={(e) => setName(e.target.value)}
+        />
       </FormControl>
     </VStack>
   );
