a2024e49a7053d994c9dd4df9efc21b77ce2baf5
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-10-31 13:55:03 +0300

    adding routes for api endpoint

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,6 @@ app.get("/", (req, res) => {
   res.send("Your first Api in express running succesfully ");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 app.listen(5000, console.log("Server started on PORT 5000".green.bold));
