b4ad444c61178cfdd8c4b98838f25ec6f7a1e543
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-11-03 09:00:16 +0300

    validation user, email,password

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,20 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async () => {
   const { name, password, email, pic } = req.body;
+
+  if (!name || !password || !email) {
+    res.status(400);
+
+    throw new Error("Please enter the required fields");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.send(400);
+
+    throw new Error("User already exists");
+  }
 });
