19d23a8ab3ed3e1035fd29a51553fde104d8378c
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-11-03 08:38:45 +0300

    adding routes path

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-// router.route("/login").get()
+router.route("/login").post(registerUser);
+
+router.post("/login", authUser);
 
 module.exports = router;
