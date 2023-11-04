2d99cc9ba3d7538912ee0bba298418efbe6c83a5
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-11-04 12:56:57 +0300

    export auth function

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -39,27 +39,26 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-const authUser = asyncHandler(async(req, res) => {
-    const {email, password} = req.body 
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    if(! email || ! password){
-        throw new Error("Please enter the fields ")
-    }
+  if (!email || !password) {
+    throw new Error("Please enter the fields ");
+  }
 
-    const user = await User.findOne({email})
+  const user = await User.findOne({ email });
 
-    if (user && ()){
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            token:generateToken(user._id)
-        })
-    }else{
-      res.status(401);
-      throw new Error("Invalid email and password");
-    }
+  if (user && (await User.matchPaswword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email and password");
+  }
+});
 
-})
-
-module.exports = { registerUser };
+module.exports = { registerUser, authUser };
