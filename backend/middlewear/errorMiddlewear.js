2dfd05cf847b030216bb1a3fec3809c03709479c
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-11-08 16:32:36 +0300

    error handler function

---
+++ backend/middlewear/errorMiddlewear.js
@@ -0,0 +1,9 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found ${req.originalUrl}`);
+  res.status(401);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : statusCode;
+};
