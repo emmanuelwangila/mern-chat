d92583592e7b82db7b2bc72cad237b17145dc101
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-07-20 00:24:44 +0300

    export the accesCht controllers..

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -15,5 +15,35 @@ const accessChat = asyncHandler(async (req, res, next) => {
       { users: { $elemMatch: { $eq: req.user._id } } },
       { users: { $elemMatch: { $eq: userId } } },
     ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
   });
+
+  if (isChat.length > o) {
+    res.send(isChat[0]);
+  } else {
+    var chatData = {
+      chatName: "sender",
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    };
+
+    try {
+      const createdChat = await Chat.create(chatData);
+      const fullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+      );
+      res.status(200).send(fullChat);
+    } catch (error) {
+      res.status(400);
+      throw new Error(error.message);
+    }
+  }
 });
+
+module.exports = { accessChat };
