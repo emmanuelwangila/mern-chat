95125aa1895d0a89da7d73de3a14360eb45df37a
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-07-19 22:57:29 +0300

    auth middlewear to slve the errors..

--- backend/middlewear/authMiddlewear.js
+++ backend/middlewear/authMiddlewear.js
@@ -1,3 +1,19 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel.js");
 const asyncHandler = require("express-async-hanler");
+
+const protect = asyncHandler((req, res, next) => {
+  let token;
+
+  if (
+    req.authorization.headers &&
+    req.authorization.headers.startsWith("Bearer")
+  ) {
+    try {
+      token = req.authorization.headers.split("")[1];
+
+      // decode the token
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {}
+  }
+});
