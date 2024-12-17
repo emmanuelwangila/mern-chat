b5d9485d974af6f0bb24229314d340922f11be1a
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-12-17 16:36:25 +0300

    Add route for creating the group chat

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, getAllChats);
-// router.route("/group").post(protect, createGroupChat);
+router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroupChat);
 // router.route("/remove").delete(protect, deleteGroupChat);
 // router.route("/addgroup").put(protect, addGroupChat);
