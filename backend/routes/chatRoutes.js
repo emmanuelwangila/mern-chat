f92f48977b1534be3b7911c330961d563937a53c
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-12-17 15:52:00 +0300

    add a route to get all chats

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -5,7 +5,7 @@ const { accessChat } = require("../controllers/chatControllers");
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChat);
+router.route("/").get(protect, getAllChats);
 // router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroupChat);
 // router.route("/remove").delete(protect, deleteGroupChat);
