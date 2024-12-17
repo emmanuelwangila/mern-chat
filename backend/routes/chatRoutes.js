996b27a637a9261f69dc036cb4830ac55cb8ee97
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-12-17 18:30:16 +0300

    update the chat endpoint for renaming

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, getAllChats);
 router.route("/group").post(protect, createGroupChat);
-router.route("/rename").put(protect, updateGroupChat);
+router.route("/group/rename").put(protect, updateGroupChat);
 // router.route("/remove").delete(protect, deleteGroupChat);
 // router.route("/addgroup").put(protect, addGroupChat);
 
