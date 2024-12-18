b99db2faa6123c13ee15c559e13913f728bf2fb8
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-12-19 01:04:04 +0300

    refactor the chats page cleanups

--- frontend/src/Pages/Chat.jsx
+++ frontend/src/Pages/Chat.jsx
@@ -3,25 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function Chat() {
-  const [chats, setChats] = useState([]);
-
-  const fetchChats = async () => {
-    const { data } = await axios.get("/api/chat");
-    setChats(data);
-  };
-
-  useEffect(() => {
-    fetchChats();
-  }, []);
-
-  return (
-    <div>
-      {" "}
-      {chats.map((chat) => (
-        <div key={chat._id}> {chat.chatName} </div>
-      ))}{" "}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Chat;
