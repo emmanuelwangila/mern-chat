fb563d8b2efa411c9e0e7ee2bda34fd8aaed4974
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-12-18 20:42:25 +0300

    export global state management file

--- frontend/src/Context/ChatProvider.jsx
+++ frontend/src/Context/ChatProvider.jsx
@@ -1,3 +1,10 @@
+import { connect } from "mongoose";
 import { createContext } from "react";
 
 const chatContext = createContext();
+
+const chatProvider = ({ children }) => {
+  <chatContext.Provider>{children}</chatContext.Provider>;
+};
+
+export default connect(chatProvider);
