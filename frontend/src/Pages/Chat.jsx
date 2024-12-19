ccada999c42aa220fb5390348022b3fec9e91ec1
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-12-19 22:18:58 +0300

    configure the chats ui structure

--- frontend/src/Pages/Chat.jsx
+++ frontend/src/Pages/Chat.jsx
@@ -1,12 +1,18 @@
 // import React, { useEffect } from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useChatState } from "../context/chatProvider";
+import { useChatState } from "../Context/ChatProvider";
 
 function Chat() {
   const { user } = useChatState();
 
-  return <div></div>;
+  return (
+    <div className="w-[100%] bg-slate-100 m-3 p-3 rounded-md text-blue-500">
+      {user && <SideBar />}
+      {user && <ChatBox />}
+      {user && <ChatInputBody />}
+    </div>
+  );
 }
 
 export default Chat;
