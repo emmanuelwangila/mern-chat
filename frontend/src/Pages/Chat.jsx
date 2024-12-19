b3cc4921373c50d326599a4172572495a4c15dee
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-12-19 22:07:30 +0300

    import global state management on chat UI

--- frontend/src/Pages/Chat.jsx
+++ frontend/src/Pages/Chat.jsx
@@ -1,8 +1,11 @@
 // import React, { useEffect } from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useChatState } from "../context/chatProvider";
 
 function Chat() {
+  const { user } = useChatState();
+
   return <div></div>;
 }
 
