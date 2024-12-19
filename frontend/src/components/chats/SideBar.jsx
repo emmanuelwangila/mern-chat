c8728a70acddf5d82143ff9a31020cdb6acaa559
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-12-19 22:38:37 +0300

    component ofr the iseBar

---
+++ frontend/src/components/chats/SideBar.jsx
@@ -0,0 +1,13 @@
+import { React } from "react";
+
+const SideBar = () => {
+  return (
+    <div className="w-full h-full m-2 p-2">
+      <ul>
+        <li>chat</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideBar;
