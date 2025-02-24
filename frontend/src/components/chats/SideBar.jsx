737bcd2552b546261135fdd550d9629ecb081986
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-02-24 12:57:33 +0300

    intalling the dep on correct folder

--- frontend/src/components/chats/SideBar.jsx
+++ frontend/src/components/chats/SideBar.jsx
@@ -1,17 +1,25 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 const SideBar = () => {
   const [search, setSearch] = useState("");
   const [result, setResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadChat, setLoadChat] = useState();
+
   return (
     <div className="w-full h-full m-2 p-2">
-      <input
-        type="text"
-        className="w-[20%] h-4 m-2 p-3 rounded-md "
-        placeholder="search chats"
-      ></input>
+      <div className="relative">
+        <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="w-[20%] h-10 pl-10 pr-4 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+          placeholder="Search chats"
+        />
+      </div>
       <h1>SideBar</h1>
     </div>
   );
