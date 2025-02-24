99509a616c5f2e905a44bbfb94c82ffe41b3d345
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-02-24 12:29:28 +0300

    adding states for importing the chats

--- frontend/src/components/chats/SideBar.jsx
+++ frontend/src/components/chats/SideBar.jsx
@@ -1,8 +1,17 @@
-import { React } from "react";
+import { React, useState } from "react";
 
 const SideBar = () => {
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadChat, setLoadChat] = useState();
   return (
     <div className="w-full h-full m-2 p-2">
+      <input
+        type="text"
+        className="w-[20%] h-4 m-2 p-3 rounded-md "
+        placeholder="search chats"
+      ></input>
       <h1>SideBar</h1>
     </div>
   );
