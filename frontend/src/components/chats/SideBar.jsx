61d412ac52faf32f59b2ff9eb7556b317a666bf7
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-04-01 21:00:46 +0300

    Styling the chats UI page

--- frontend/src/components/chats/SideBar.jsx
+++ frontend/src/components/chats/SideBar.jsx
@@ -8,19 +8,20 @@ const SideBar = () => {
   const [loadChat, setLoadChat] = useState();
 
   return (
-    <div className="flex w-full h-full m-2 p-2">
-      <div className="relative">
-        <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-
+    <div className="lg:flex-col md:flex-col sm:flex-wrap  w-full h-full m-2 p-2">
+      <div className="text-yellow-500 m-1 flex justify-center  font-sans  p-1 ">
+        Bumble Chat
+      </div>
+      <div className="relative ">
+        <FaSearch className="left-3 absolute m-4 p-0.5   transform -translate-y-1/2 text-gray-400" />
         <input
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className="w-[60%] h-10 pl-10 pr-4 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
-          placeholder="Search chats font-sans "
+          className="w-[30%] h-10 pl-12  font-sans rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+          placeholder="Search chats  "
         />
       </div>
-      <div className="text-yellow-500 m-1 font-sans  p-3 ">Bumble Chat</div>
     </div>
   );
 };
